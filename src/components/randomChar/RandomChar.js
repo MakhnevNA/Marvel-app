389febc001e2e896cc8d4f60bb8d087a8eb904b2
Nikita <nikitka.makhnev@mail.ru>
2023-07-08 17:02:13 +0500

    удалил баг с двойным запросом на сервер

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,11 +8,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-	constructor(props) {
-		super(props);
-		this.updateChar()
-
-	}
 
 	state = {
 		char: {},
@@ -21,6 +16,11 @@ class RandomChar extends Component {
 	}
 
 	marvelService = new MarvelService()
+
+
+	componentDidMount() {
+		this.updateChar()
+	}
 
 	onCharLoaded = (char) => {
 		this.setState({
