472b0a756bd92cecfc7a0176ffea3eceeb787135
Nikita <nikitka.makhnev@mail.ru>
2023-08-22 10:54:34 +0500

    Создал собственный хук по запросу на сервер

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage'
 
@@ -11,11 +11,8 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
 
 	const [char, setChar] = useState({})
-	const [loading, setLoading] = useState(true)
-	const [error, setError] = useState(false)
 
-
-	const marvelService = new MarvelService()
+	const {loading, error, getCharacter, clearError} = useMarvelService()
 
 	useEffect(() => {
 		updateChar()
@@ -24,28 +21,17 @@ const RandomChar = () => {
 
 	const onCharLoaded = (char) => {
 		setChar(char)
-		setLoading(false)
-	}
-
-	const onCharLoading = () => {
-		setLoading(true)
-	}
-
-	const onError = () => {
-		setLoading(false)
-		setError(true)
 	}
 
 
 	const updateChar = () => {
 
-		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
-		
-		onCharLoading()
+		clearError()
 
-		marvelService.getCharacter(id)
+		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+
+		getCharacter(id)
 			.then(onCharLoaded)
-			.catch(onError)
 	}
 
 	
@@ -84,9 +70,9 @@ const View = ({char}) => {
 	const { name, description, thumbnail, homepage, wiki } = char;
 	let stl = null
 
-	if (thumbnail.indexOf('image_not_available') !== -1) {
-		stl = {objectFit: 'contain'}
-	}
+	// if (thumbnail.indexOf('image_not_available') !== -1) {
+	// 	stl = {objectFit: 'contain'}
+	// }
 
 	return (
 		<div className="randomchar__block">
