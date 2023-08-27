c12b046649b8116fa3e1bd18beffcc2c4210771b
Nikita <nikitka.makhnev@mail.ru>
2023-08-27 20:48:31 +0500

    Добавлена маршрутизация react router 6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,45 +1,28 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
 
 import AppHeader from "../appHeader/AppHeader";
-// import RandomChar from "../randomChar/RandomChar";
-// import CharList from "../charList/CharList";
-// import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicList from "../comicsList/ComicsList"
 
-import decoration from '../../resources/img/vision.png';
+
 
 const App = () => {
 
-	const [selectedChar, setChar] = useState(null)
-
-	const onCharSelected = (id) => {
-		setChar(id)
-	}
 
 	return (
-		<div className="app">
-			<AppHeader/>
-			<main>
-				{/* <ErrorBoundary>
-					<RandomChar />
-				</ErrorBoundary>
-				 */}
-				
-				<ErrorBoundary>
-					<ComicList></ComicList>
-				</ErrorBoundary>
-				{/* <div className="char__content">
-					<ErrorBoundary>
-						<CharList onCharSelected={onCharSelected} />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<CharInfo charId={selectedChar} />
-					</ErrorBoundary>
-				</div> */}
-				{/* <img className="bg-decoration" src={decoration} alt="vision"/> */}
-			</main>
-		</div>
+		<Router>
+			<div className="app">
+				<AppHeader/>
+				<main>
+					<Routes>
+						<Route path="/" element={<MainPage></MainPage>}/>
+						<Route path="/comics" element={<ComicsPage></ComicsPage>}/>
+					</Routes>
+					
+				</main>
+			</div>
+		</Router>
 	)
 }
 
---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,18 @@
+
+import AppBanner from '../appBanner/AppBanner'
+import ComicList from "../comicsList/ComicsList"
+
+
+const ComicsPage = () => {
+	
+
+
+	return (
+		<>
+			<AppBanner></AppBanner>
+			<ComicList></ComicList>
+		</>
+	)
+}
+
+export default ComicsPage
