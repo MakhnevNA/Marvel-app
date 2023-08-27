d1dfd55c2cb722fea3f2a006fdb3ea139b789801
Nikita <nikitka.makhnev@mail.ru>
2023-08-27 18:10:44 +0500

    Сделал загрузку комиксов

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
+// import RandomChar from "../randomChar/RandomChar";
+// import CharList from "../charList/CharList";
+// import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import ComicList from "../comicsList/ComicsList"
 
 import decoration from '../../resources/img/vision.png';
 
@@ -20,19 +21,23 @@ const App = () => {
 		<div className="app">
 			<AppHeader/>
 			<main>
-				<ErrorBoundary>
+				{/* <ErrorBoundary>
 					<RandomChar />
 				</ErrorBoundary>
+				 */}
 				
-				<div className="char__content">
+				<ErrorBoundary>
+					<ComicList></ComicList>
+				</ErrorBoundary>
+				{/* <div className="char__content">
 					<ErrorBoundary>
 						<CharList onCharSelected={onCharSelected} />
 					</ErrorBoundary>
 					<ErrorBoundary>
 						<CharInfo charId={selectedChar} />
 					</ErrorBoundary>
-				</div>
-				<img className="bg-decoration" src={decoration} alt="vision"/>
+				</div> */}
+				{/* <img className="bg-decoration" src={decoration} alt="vision"/> */}
 			</main>
 		</div>
 	)
